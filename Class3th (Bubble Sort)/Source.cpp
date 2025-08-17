512fb4cdf5223bc679add18f6918fc899794c24d
jiseok08 <heoji0912@gmail.com>
2025-08-17 15:20:44 +0900

    (2025.8.14) 거품 정렬

---
+++ Class3th (Bubble Sort)/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 거품 정렬
+    // 서로 인접한 두 원소를 비교하여 정렬하는 알고리즘입니다.
+
+
+    int list[] = { 3,5,6,1,2,7,4 };
+
+    int size = sizeof(list) / sizeof(list[0]);
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < (size - i - 1); j++)
+        {
+            if (list[j] > list[j + 1])
+            {
+                std::swap(list[j], list[j + 1]);
+            }
+        }
+    }
+
+    for (const auto& element : list)
+    {
+        cout << element << " ";
+    }
+
+#pragma endregion
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,8 +2,53 @@
 
 using namespace std;
 
+// 1. 두 개의 자연수 x, y가 있을 때
+//    x와 y를 나눈 나머지를 r이라고 합니다.
+
+// 2. r이 0이라면, x와 y의 최대공약수는 y가 됩니다.
+
+// 3. r이 0이 아니라면, x와 y의 최대공약수는 y와 r의
+//    최대 공약수와 같기 때문에 x에는 y 그리고 y에는
+//    r을 대입한 후 1번으로 돌아갑니다.
+
+int Euclid(int x, int y)
+{
+    int r = x % y;
+
+    if (r == 0)
+    {
+        return y;
+    }
+    else
+    {
+        Euclid(y, r);
+    }
+}
+
 int main()
 {
+#pragma region 유클리드 호제법
+    // 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+    // 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+    int x = 24;
+    int y = 36;
+
+    int result = 0;
+
+    for (int i = 1; i <= x && i <= y; i++)
+    {
+        if (x % i == 0 && y % i == 0)
+        {
+            result = i;
+        }
+    }
+
+    cout << "최대공약수 : " << result << endl;
+
+    cout << "유클리드 : " << Euclid(x, y) << endl;
+
+#pragma endregion
 
     return 0;
 }
